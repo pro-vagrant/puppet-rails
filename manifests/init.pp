9824bee32b74f1045f9e5085a0b87e276b626dc3
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2014-05-27 11:32:58 +0200

    Replace deprecated options with --no-documentation

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,20 +8,20 @@ class rails {
     package { 'gem': ensure => present }
   }
 
-  exec { 'rails::gem-sqlite3':
-    command => "gem install --no-ri --no-rdoc sqlite3 -v '1.3.9'",
-    path    => '/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin',
-    require => [Package['gem', 'libsqlite3-dev']]
-  }
-
   exec { 'rails::gem-update':
     command => "gem update --system",
     path    => '/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin',
-    require => [Package['gem'], Exec['rails::gem-sqlite3']]
+    require => [Package['gem']]
+  }
+
+  exec { 'rails::gem-sqlite3':
+    command => "gem install --no-document sqlite3 -v '1.3.9'",
+    path    => '/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin',
+    require => [Package['gem', 'libsqlite3-dev'], Exec['rails::gem-update']]
   }
 
   exec { 'rails::rails':
-    command => "gem install --no-ri --no-rdoc rails",
+    command => "gem install --no-document rails",
     timeout => 6000,
     path    => '/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin',
     require => [Exec['rails::gem-update']]
