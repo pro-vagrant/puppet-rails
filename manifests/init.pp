92ab726d8b8674d036c32107de42418f3932d392
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2014-05-28 09:50:30 +0200

    Without sqlite3

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,36 +1,31 @@
 class rails {
 
-  if defined(Package['libsqlite3-dev']) == false {
-    package { 'libsqlite3-dev': ensure => present }
-  }
+#  if defined(Package['libsqlite3-dev']) == false {
+#    package { 'libsqlite3-dev': ensure => present }
+#  }
 
   if defined(Package['gem']) == false {
     package { 'gem': ensure => present }
   }
 
-  exec { 'rails::apt-get-update':
-    command => "apt-get update -y",
-    path    => '/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin'
-  }
-
   exec { 'rails::gem-update':
     command => "gem update --system",
     path    => '/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin',
-    require => [Exec['rails::apt-get-update'], Package['gem']]
+    require => [Package['gem']]
   }
 
-  exec { 'rails::gem-sqlite3':
-    #command => "gem install --no-document sqlite3 -v '1.3.9'",
-    command => "gem install sqlite3 -v '1.3.9'",
-    path    => '/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin',
-    require => [Package['gem', 'libsqlite3-dev'], Exec['rails::gem-update']]
-  }
+#  exec { 'rails::gem-sqlite3':
+#    #command => "gem install --no-document sqlite3 -v '1.3.9'",
+#    command => "gem install sqlite3 -v '1.3.9'",
+#    path    => '/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin',
+#    require => [Package['gem', 'libsqlite3-dev'], Exec['rails::gem-update']]
+#  }
 
   exec { 'rails::rails':
     command => "gem install rails",
     timeout => 6000,
     path    => '/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin',
-    require => [Exec['rails::gem-update', 'rails::gem-sqlite3']]
+    require => [Exec['rails::gem-update']]
   }
 
 }
